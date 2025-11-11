52517a775474037f079e06205334369ec8d48999
Daniela Menezes <dani.a.menezes@outlook.com>
2025-11-11 14:37:50 -0300

    primeira versão

---
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,16 @@
+alert('Boas vidas ao jogo do numero secreto');
+let  numeroSecreto = 9;
+let chute = prompt('escolha um número entre 1 e 30')
+console.log(numeroSecreto)
+
+//se o chute for igual ao numero secreto
+
+if( chute == numeroSecreto) {
+ alert(`haha, você descobriu o numero secreto ${numeroSecreto}`);
+}
+
+// se o chute for diferente do numero secreto 
+
+else {
+    alert('vixiiiii erou ein ')
+}
