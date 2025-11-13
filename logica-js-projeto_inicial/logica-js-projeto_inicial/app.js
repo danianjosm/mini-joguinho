33e290cc9379eaa9d123fb8f3297bcaeb3291d27
Daniela Menezes <dani.a.menezes@outlook.com>
2025-11-13 20:53:58 -0300

    contagem de tentativas

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,22 +1,27 @@
 alert('Boas vidas ao jogo do numero secreto');
 let  numeroSecreto = 9;
 let chute 
-console.log(numeroSecreto)
+let tentativas = 1; 
+console.log(numeroSecreto);
 
 // while é o equivalente a enquanto 
 
 while (chute != numeroSecreto){
-    chute = prompt('escolha um numero entre1 e 30 ');
-}
+    chute = prompt('escolha um numero entre 1 e 30 ');
 
-if( chute == numeroSecreto) {
-   alert(`haha, você descobriu o numero secreto ${numeroSecreto}`);
+  // se o chute for igual ao numero secreto 
+  if( chute == numeroSecreto) {
+   alert(`haha, você descobriu o numero secreto ${numeroSecreto}com ${tentativas} tentativas`);
 }
-    // se o chute for diferente do numero secreto 
-else {
-    if (numeroSecreto > chute){
-        alert (`o numero secreto é maior que ${chute}`);
-    } else {
-        alert('o numero secreto é menor ');
-    }
+  // se o chute for diferente do numero secreto 
+   else {
+     if (numeroSecreto > chute){
+         alert (`o numero secreto é maior que ${chute}`);
+ } else {
+         alert('o numero secreto é menor ');
+     }
+     // tentativas +1 
+     tentativas ++;
+ }
+
 }
